ee357e3e95fe87f5b9adbdd5be31d296ebe36e96
Arun <arunjayas123@gmail.com>
2023-03-12 21:44:58 +0530

    Commit done from GITFolder1inarun folder which is in C drive

---
+++ src/main/java/Group1/Artifact1/Class1.java
@@ -0,0 +1,12 @@
+package Group1.Artifact1;
+
+public class Class1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This line is written in Workspace1GIT in Artifact1 Project");
+
+	}
+
+}
